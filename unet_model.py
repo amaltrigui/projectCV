40d95c96f91ddb12613e58338ef239e16207736d
zeinebBC <benchaabenzeineb@gmail.com>
2022-11-08 11:51:37 +0800

    just trying th commit procedure, update the unet_model and add the loss functions

--- unet_model.py
+++ unet_model.py
@@ -12,15 +12,15 @@ from torch.nn import functional as F
 
 class ConvBlock(nn.Module):
     """
-    A Convolutional Block that consists of two convolution layers each followed by
-    instance normalization, LeakyReLU activation and dropout.
+    A Convolutional Block consisting of the following layers: 
+    [3*3 convolution layer --> instance normalization --> LeakyReLU activation--> dropout] * 2
     """
 
     def __init__(self, in_chans, out_chans, drop_prob):
         """
         Args:
-            in_chans (int): Number of channels in the input.
-            out_chans (int): Number of channels in the output.
+            in_chans (int): Number of the input channels.
+            out_chans (int): Number of the output channels.
             drop_prob (float): Dropout probability.
         """
         super().__init__()
@@ -30,13 +30,13 @@ class ConvBlock(nn.Module):
         self.drop_prob = drop_prob
 
         self.layers = nn.Sequential(
-            nn.Conv2d(in_chans, out_chans, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_chans, out_chans, kernel_size=3, padding=1, bias=False), #try with bias 
             nn.InstanceNorm2d(out_chans),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),    #try other slopes 
             nn.Dropout2d(drop_prob),
-            nn.Conv2d(out_chans, out_chans, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(out_chans, out_chans, kernel_size=3, padding=1, bias=False), #try with bias 
             nn.InstanceNorm2d(out_chans),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),   #try other slopes 
             nn.Dropout2d(drop_prob)
         )
 
@@ -50,22 +50,18 @@ class ConvBlock(nn.Module):
         """
         return self.layers(input)
 
-    def __repr__(self):
-        return f'ConvBlock(in_chans={self.in_chans}, out_chans={self.out_chans}, ' \
-            f'drop_prob={self.drop_prob})'
 
 
-class TransposeConvBlock(nn.Module):
+class UpConvBlock(nn.Module):
     """
-    A Transpose Convolutional Block that consists of one convolution transpose layers followed by
-    instance normalization and LeakyReLU activation.
+    A Transpose Convolutional Block consisting of : 2x2 deconvolution layer --> instance normalization--> LeakyReLU activation.
     """
 
     def __init__(self, in_chans, out_chans):
         """
         Args:
-            in_chans (int): Number of channels in the input.
-            out_chans (int): Number of channels in the output.
+            in_chans (int): Number of the input channels.
+            out_chans (int): Number of the output channels.
         """
         super().__init__()
 
@@ -75,7 +71,7 @@ class TransposeConvBlock(nn.Module):
         self.layers = nn.Sequential(
             nn.ConvTranspose2d(in_chans, out_chans, kernel_size=2, stride=2, bias=False),
             nn.InstanceNorm2d(out_chans),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True), #try other slopes
         )
 
     def forward(self, input):
@@ -88,8 +84,7 @@ class TransposeConvBlock(nn.Module):
         """
         return self.layers(input)
 
-    def __repr__(self):
-        return f'ConvBlock(in_chans={self.in_chans}, out_chans={self.out_chans})'
+ 
 
 
 class UnetModel(nn.Module):
@@ -102,43 +97,42 @@ class UnetModel(nn.Module):
         computing and computer-assisted intervention, pages 234–241. Springer, 2015.
     """
 
-    def __init__(self, in_chans, out_chans, chans, num_pool_layers, drop_prob):
+    def __init__(self, in_chans, out_chans, first_chans, num_blocks, drop_prob):
         """
         Args:
-            in_chans (int): Number of channels in the input to the U-Net model.
-            out_chans (int): Number of channels in the output to the U-Net model.
-            chans (int): Number of output channels of the first convolution layer.
-            num_pool_layers (int): Number of down-sampling and up-sampling layers.
+            in_chans (int): Number of the input channels of the whole U-Net model.
+            out_chans (int): Number of the output channels of the whole U-Net model.
+            first_chans (int): Number of the output channels of the first convolution layer.
+            num_blocks (int): Number of the down-sampling blocks = Number of the up-sampling blocks.
+            => total number of blocks: 2xnum_blocks + 1 (the bottom block connecting both sides)
             drop_prob (float): Dropout probability.
         """
         super().__init__()
 
         self.in_chans = in_chans
         self.out_chans = out_chans
-        self.chans = chans
-        self.num_pool_layers = num_pool_layers
+        self.first_chans = first_chans 
+        self.num_blocks = num_blocks
         self.drop_prob = drop_prob
 
-        self.down_sample_layers = nn.ModuleList([ConvBlock(in_chans, chans, drop_prob)])
-        ch = chans
-        for i in range(num_pool_layers - 1):
-            self.down_sample_layers += [ConvBlock(ch, ch * 2, drop_prob)]
-            ch *= 2
-        self.conv = ConvBlock(ch, ch * 2, drop_prob)
+        self.down_layers = nn.ModuleList([ConvBlock(self.in_chans,self.first_chans, drop_prob)])
 
-        self.up_conv = nn.ModuleList()
-        self.up_transpose_conv = nn.ModuleList()
-        for i in range(num_pool_layers - 1):
-            self.up_transpose_conv += [TransposeConvBlock(ch * 2, ch)]
-            self.up_conv += [ConvBlock(ch * 2, ch, drop_prob)]
+        ch = self.first_chans 
+        for i in range(num_blocks - 1):
+            self.down_layers += [ConvBlock(ch, ch * 2, drop_prob)]
+            ch *= 2
+
+        self.down_up_conv = ConvBlock(ch, ch * 2, drop_prob)
+        ch *= 2
+
+        self.up_layers = nn.ModuleList()
+        self.up_deconv_layers = nn.ModuleList()
+        for i in range(num_blocks ): 
+            self.up_deconv_layers += [UpConvBlock(ch , ch//2)]
+            self.up_layers += [ConvBlock(ch, ch//2, drop_prob)]
             ch //= 2
 
-        self.up_transpose_conv += [TransposeConvBlock(ch * 2, ch)]
-        self.up_conv += [
-            nn.Sequential(
-                ConvBlock(ch * 2, ch, drop_prob),
-                nn.Conv2d(ch, self.out_chans, kernel_size=1, stride=1),
-            )]
+        self.final_layer = nn.Conv2d(ch, self.out_chans, kernel_size=1) 
 
     def forward(self, input):
         """
@@ -152,28 +146,24 @@ class UnetModel(nn.Module):
         output = input
 
         # Apply down-sampling layers
-        for i, layer in enumerate(self.down_sample_layers):
+        for i, layer in enumerate(self.down_layers):
             output = layer(output)
             stack.append(output)
-            output = F.avg_pool2d(output, kernel_size=2, stride=2, padding=0)
+            output = F.avg_pool2d(output, kernel_size=2, stride=2, padding=0)  #try maxpooling 
 
-        output = self.conv(output)
+        output = self.down_up_conv(output)
 
         # Apply up-sampling layers
-        for transpose_conv, conv in zip(self.up_transpose_conv, self.up_conv):
-            downsample_layer = stack.pop()
-            output = transpose_conv(output)
+        for deconv, conv in zip(self.up_deconv_layers, self.up_layers):
+            down_input = stack.pop()
+            output = deconv(output)
 
-            # Reflect pad on the right/botton if needed to handle odd input dimensions.
-            padding = [0, 0, 0, 0]
-            if output.shape[-1] != downsample_layer.shape[-1]:
-                padding[1] = 1 # Padding right
-            if output.shape[-2] != downsample_layer.shape[-2]:
-                padding[3] = 1 # Padding bottom
-            if sum(padding) != 0:
-                output = F.pad(output, padding, "reflect")
-
-            output = torch.cat([output, downsample_layer], dim=1)
+            diff_H = down_input.size()[2] - output.size()[2]
+            diff_W = down_input.size()[3] - output.size()[3]
+            output = F.pad(output, [diff_H // 2, diff_H - diff_H // 2, diff_W // 2, diff_W - diff_W // 2])  #check this if bug with padding 
+           
+            output = torch.cat([output, down_input], dim=1)
             output = conv(output)
+        output = self.final_layer(output)
 
         return output

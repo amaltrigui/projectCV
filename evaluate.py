cfd71ced434e2280179423b2d21948f73fa538b2
zeinebBC <benchaabenzeineb@gmail.com>
2022-11-08 11:53:40 +0800

    add evaluate file

---
+++ evaluate.py
@@ -0,0 +1,87 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+import argparse
+import pathlib
+from argparse import ArgumentParser
+
+import h5py
+import numpy as np
+from runstats import Statistics
+from skimage.measure import compare_psnr, compare_ssim
+
+
+#each output or target is an image or an image volume (tensor with shape C x H x W ?)
+def mse(target, output):
+    """ Compute Mean Squared Error (MSE) """
+    return np.mean((target - output) ** 2)
+
+
+def nmse(target, output):
+    """ Compute Normalized Mean Squared Error (NMSE) """
+    return np.linalg.norm(target - output) ** 2 / np.linalg.norm(target) ** 2
+
+
+def psnr(target, output):
+    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
+    return compare_psnr(target, output, data_range=target.max())  
+    # data_range in numerator 
+    #Changed in version 0.16: This function was renamed from skimage.measure.compare_psnr to skimage.metrics.peak_signal_noise_ratio
+
+
+
+def ssim(target, output):
+    """ Compute Structural Similarity Index Metric (SSIM). """
+    return compare_ssim( target, output, channel_axis=0, data_range=target.max())
+    # default values for k1=0.01;k2=0.03,window_size=7 
+    #Changed in version 0.16: This function was renamed from skimage.measure.compare_ssim to skimage.metrics.structural_similarity
+
+def l1(target, output):
+    """ Compute Mean Absolute Error (L1). """
+    return np.mean(np.abs(target-output))
+
+
+METRIC_FUNCS = dict(
+    MSE=mse,
+    NMSE=nmse,
+    PSNR=psnr,
+    SSIM=ssim,
+    L1=l1, 
+    
+)
+
+class Metrics:
+    """
+    Maintains running statistics for a given collection of metrics.
+    """
+
+    def __init__(self, metric_funcs):
+        self.metrics = {
+            metric: Statistics() for metric in metric_funcs
+        }
+
+    def push(self, target, recons):
+        for metric, func in METRIC_FUNCS.items():
+            self.metrics[metric].push(func(target, recons))
+
+    def means(self):
+        return {
+            metric: stat.mean() for metric, stat in self.metrics.items()
+        }
+
+    def stddevs(self):
+        return {
+            metric: stat.stddev() for metric, stat in self.metrics.items()
+        }
+
+    def __repr__(self):
+        means = self.means()
+        stddevs = self.stddevs()
+        metric_names = sorted(list(means))
+        return ' '.join(
+            f'{name} = {means[name]:.4g} +/- {2 * stddevs[name]:.4g}' for name in metric_names
+        )
